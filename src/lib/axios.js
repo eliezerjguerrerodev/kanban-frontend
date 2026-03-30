e96e758d4ef90cd58dc761185b04ce3f0cb59eb8
Eliezer J. Guerrero <eliezerjguerrero@gmail.com>
2026-03-30 15:09:33 -0400

    fix: ensure API URL has protocol to fix Vercel deploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,12 @@
 import Axios from 'axios';
 
+let apiURL = import.meta.env.VITE_API_URL || 'http://localhost:8085';
+if (apiURL && !apiURL.startsWith('http')) {
+  apiURL = 'https://' + apiURL;
+}
+
 const axios = Axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8085',
+  baseURL: apiURL,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
